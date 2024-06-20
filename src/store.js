6a3b4f2b3172213adc9061fbd5af10ebd19ccabb
ChaosMiPC <ChaosJape@163.com>
2024-06-20 16:08:40 +0800

    feat: introducing Vuex to manage state

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+    state() {
+        return {
+            activeMenu: localStorage.getItem('activeMenu') || '/home',
+        };
+    },
+    mutations: {
+        setActiveMenu(state, menu) {
+            state.activeMenu = menu;
+            localStorage.setItem('activeMenu', menu);
+        },
+    },
+    actions: {
+        updateActiveMenu({ commit }, menu) {
+            commit('setActiveMenu', menu);
+        },
+    },
+});
+
+export default store;
