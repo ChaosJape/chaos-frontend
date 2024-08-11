ab3f5c6533b5d78f6327a23707a82d2d9aaeaf62
ChaosMiPC <ChaosJape@163.com>
2024-08-12 01:30:15 +0800

    feat: extract global components

---
+++ src/components/global/header/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-header style="background:skyblue;">It's gonna be the header</el-header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/global/sidebar/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-aside width="200px">
+    <el-menu :default-active="activeMenu" @select="handleSelect" router>
+      <el-menu-item index="/">Home</el-menu-item>
+      <el-menu-item index="/text">Text</el-menu-item>
+      <el-menu-item index="/selector">Selector</el-menu-item>
+      <el-menu-item index="/display">Display</el-menu-item>
+      <el-menu-item index="/background">Background</el-menu-item>
+      <el-menu-item index="/box-model">BoxModel</el-menu-item>
+      <el-menu-item index="/float">Float</el-menu-item>
+      <el-menu-item index="/about">About</el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script setup>
+import store from "@/store/index.js";
+import {computed} from "vue";
+const activeMenu = computed(() => store.state.activeMenu);
+function handleSelect(key, keyPath) {
+  console.log(`Selected menu item: ${key}`);
+  store.dispatch('updateActiveMenu', key);
+}
+</script>
+
+<style scoped>
+
+</style>
