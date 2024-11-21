37a1cb1a63c6720f4096147bf055b13110b1d95b
ChaosJape <ChaosJape@163.com>
2024-11-21 15:39:19 +0800

    fix: fix a bug about path

--- src/components/global/sidebar/index.vue
+++ src/components/global/sidebar/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-aside width="200px">
     <el-menu :default-active="activeMenu" @select="handleSelect" router>
-      <el-menu-item index="/">Home</el-menu-item>
+      <el-menu-item index="/index">Home</el-menu-item>
       <el-menu-item index="/text">Text</el-menu-item>
       <el-menu-item index="/selector">Selector</el-menu-item>
       <el-menu-item index="/display">Display</el-menu-item>
