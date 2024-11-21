572a9208b343ff6742a9bea6c1dcfa4b83d83216
ChaosInt <ChaosJape@163.com>
2024-11-21 15:39:19 +0800

    fix: fix a bug about path

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 const routes = [
   {
-    path: '/',
+    path: '/index',
     name: 'Home',
     component: () => import('@/views/home/index.vue')
   },
