db98ad8da3a9590ead1972f3c656d820a92b9392
ChaosInt <ChaosJape@163.com>
2025-01-10 17:24:41 +0800

    feat: use routes.js as routes configuration and change menu item style

--- src/components/global/sidebar/index.vue
+++ src/components/global/sidebar/index.vue
@@ -1,22 +1,25 @@
 <template>
   <el-aside width="200px">
-    <el-menu :default-active="activeMenu" @select="handleSelect" router>
-      <el-menu-item index="/index">Home</el-menu-item>
-      <el-menu-item index="/text">Text</el-menu-item>
-      <el-menu-item index="/selector">Selector</el-menu-item>
-      <el-menu-item index="/display">Display</el-menu-item>
-      <el-menu-item index="/background">Background</el-menu-item>
-      <el-menu-item index="/box-model">BoxModel</el-menu-item>
-      <el-menu-item index="/float">Float</el-menu-item>
-      <el-menu-item index="/about">About</el-menu-item>
+    <el-menu :default-active="activeMenu" @select="handleSelect" router style="padding: 8px">
+      <el-menu-item v-for="(item, index) in menuItems.slice(1)"
+        :key="index"
+        :index="item.path"
+        style="border-radius: 8px;">
+        {{ item.name }}
+      </el-menu-item>
     </el-menu>
   </el-aside>
 </template>
 
 <script setup>
 import store from "@/store/index.js";
-import {computed} from "vue";
+import { computed, ref } from "vue";
+
 const activeMenu = computed(() => store.state.activeMenu);
+import routes from "@/router/routes.js";
+
+const menuItems = ref(routes);
+
 function handleSelect(key, keyPath) {
   console.log(`Selected menu item: ${key}`);
   store.dispatch('updateActiveMenu', key);
